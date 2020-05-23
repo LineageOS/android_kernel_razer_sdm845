49a93cce4ae521ac62dc9a662b9f058052ac3abc
Michael Bestas <mkbestas@lineageos.org>
2020-05-24 01:38:38 +0300

    drm: Import RC2 changes
    * Package version: msm-4.9-3141.tar.gz
    * Estimated base tag: LA.UM.7.3.r1-06600-sdm845.0
    
    Change-Id: I9214fdbefa6659dacb630c261cbc6fecd483aad5

--- drivers/gpu/drm/drm_modes.c
+++ drivers/gpu/drm/drm_modes.c
@@ -998,8 +998,10 @@ bool drm_mode_equal_no_clocks_no_stereo(const struct drm_display_mode *mode1,
 	    mode1->vtotal == mode2->vtotal &&
 	    mode1->vscan == mode2->vscan &&
 	    mode1->picture_aspect_ratio == mode2->picture_aspect_ratio &&
-	    (mode1->flags & ~DRM_MODE_FLAG_3D_MASK) ==
-	     (mode2->flags & ~DRM_MODE_FLAG_3D_MASK))
+	    ((mode1->flags & ~DRM_MODE_FLAG_3D_MASK) ==
+	     (mode2->flags & ~DRM_MODE_FLAG_3D_MASK)) &&
+	    ((mode1->flags & ~DRM_MODE_FLAG_DSC_BPC_MASK) ==
+	     (mode2->flags & ~DRM_MODE_FLAG_DSC_BPC_MASK)))
 		return true;
 
 	return false;
