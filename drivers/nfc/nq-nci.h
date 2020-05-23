df339ac4d9808c48748eb3aa2ab5345e2518930c
Michael Bestas <mkbestas@lineageos.org>
2020-05-24 01:38:38 +0300

    misc: Import RC2 changes
    * Package version: msm-4.9-3141.tar.gz
    * Estimated base tag: LA.UM.7.3.r1-06600-sdm845.0
    
    Change-Id: I477b94c0f7d199ddadf4f3e32b6c192f1760f3da

--- drivers/nfc/nq-nci.h
+++ drivers/nfc/nq-nci.h
@@ -47,6 +47,7 @@ enum nfcc_chip_variant {
 	NFCC_NQ_210			= 0x48,	/**< NFCC NQ210 */
 	NFCC_NQ_220			= 0x58,	/**< NFCC NQ220 */
 	NFCC_NQ_310			= 0x40,	/**< NFCC NQ310 */
+	NFCC_NQ_310_B0			= 0x41,	/**< NFCC NQ310_B0 */
 	NFCC_NQ_330			= 0x51,	/**< NFCC NQ330 */
 	NFCC_SN100_A			= 0xa3,	/**< NFCC SN100_A */
 	NFCC_SN100_B			= 0xa4,	/**< NFCC SN100_B */
